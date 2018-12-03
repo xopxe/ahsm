80385e9a6aab9dde28dd5fae14d6d35d45a66a69
Jorge Visca <jvisca@fing.edu.uy>
2018-12-03 15:52:04 -0300

    fix output for events

--- tools/debug_plain.lua
+++ tools/debug_plain.lua
@@ -33,9 +33,10 @@ M.out = function( action, p1, p2, p3, p4 )
     M.print(action..'\t'..debug_names[p1])
   elseif action == 'sched' then
     --M.print(action, p1, p2, debug_names[p3],'--'..debug_names[p4]..'->', debug_names[p4.tgt])
-    M.print(action..'\t'..(debug_names[p1] or tostring(p1))..'\t'..tostring(p2)) --called before init
+    --called before init
+    M.print(action..'\t'..(debug_names[p1] or tostring(p1))..'\t'..tostring(p2))
   elseif action == 'event' then
-    M.print(action, tostring(p1)..'\t"'..pick_debug_name(p1, p2)..'"')
+    M.print(action..tostring(p1)..'\t"'..pick_debug_name(p1, p2)..'"')
   elseif action == 'state' then
     debug_names[p1.EV_DONE] = 'EV_DONE'
     M.print(action..'\t'..tostring(p1)..'\t"'..pick_debug_name(p1, p2)..'"')
